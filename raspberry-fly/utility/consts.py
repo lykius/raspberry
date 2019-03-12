6cd48398f5336bfb66a008a2251902530fb18da2
lykius <lucadeluigi91@gmail.com>
2019-03-12 20:10:52 +0100

    Add constants definition file

---
+++ raspberry-fly/utility/consts.py
@@ -0,0 +1,40 @@
+"""
+utility/consts.py
+Constants definitions.
+"""
+
+RWALT_TOPIC = 'raw-altitude'
+FLTALT_TOPIC = 'filtered-altitude'
+RWVS_TOPIC = 'raw-vertical-speed'
+FLTVS_TOPIC = 'filtered-vertical-speed'
+LOGS_TOPIC = 'logs-topic'
+
+INITIAL_ALT = 50
+ALT_SAMPLING_INTERVAL = 0.01
+VS_CALC_DERIVATIVE = False
+VS_DERIVATIVE_NSKIP = 10
+VS_CALC_LINREG = True
+VS_LINREG_BUFFER_SIZE = 300
+
+SEND_OVER_NETWORK = True
+ALT_SERVER_ADDRESS = ('192.168.1.7', 12345)
+VS_SERVER_ADDRESS = ('192.168.1.77', 56789)
+
+LOG_DATA = False
+RWALT_LOG_FILE = './logs/raw_altitude.txt'
+FLTALT_LOG_FILE = './logs/filtered_altitude.txt'
+RWVS_LOG_FILE = './logs/raw_vspeed.txt'
+FLTVS_LOG_FILE = './logs/filtered_vspeed.txt'
+
+PROGRAM_LOG_FILE = './logs/log.txt'
+
+DEBUG = False
+SIMULATION = False
+RWALT_SIMULATION_FILE = './simulation/data/raw_altitude.txt'
+RWALT_SIMULATION_SEP = ','
+RWALT_SIMULATION_FIELD = 1
+
+DISPLAY_WRITER_NSKIP = 20
+
+ENABLE_BLUETOOTH_SERVER = False
+BLUETOOTH_NSKIP = 100
